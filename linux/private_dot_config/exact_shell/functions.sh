04eb1367d1ddafcae81944d6bc2297849be30cc1
magicWenli <48174882+magicwenli@users.noreply.github.com>
2024-03-11 01:52:31 +0000

    feat

--- linux/private_dot_config/exact_shell/functions.sh
+++ linux/private_dot_config/exact_shell/functions.sh
@@ -7,3 +7,17 @@ static() {
     printf "\e[$[RANDOM%LINES+1];$[RANDOM%COLUMNS+1]f${P[$RANDOM%5]}"
   done | lolcat
 }
+
+function pxy() {
+	export http_proxy=http://127.0.0.1:7890
+	export https_proxy=http://127.0.0.1:7890
+	export no_proxy=127.0.0.1,localhost
+	echo -e "\033[32m[√] set proxy! <http_proxy: ${http_proxy}>\033[0m"
+}
+
+function unpxy(){
+	unset http_proxy
+	unset https_proxy
+	unset no_proxy
+	echo -e "\033[31m[×] unset proxy!\033[0m"
+}
