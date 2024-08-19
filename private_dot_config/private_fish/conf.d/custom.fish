beccccb94c28a76ef016735505aab604a3c6e88c
magicwenli <48174882+magicwenli@users.noreply.github.com>
2024-08-19 03:08:00 +0000

    feat: Add git aliases for common commands

--- private_dot_config/private_fish/conf.d/custom.fish
+++ private_dot_config/private_fish/conf.d/custom.fish
@@ -1,8 +1,36 @@
 ### Aliases
 
-abbr gco "git checkout"
+abbr ga 'git add'
+abbr gaa 'git add --all'
+abbr gau 'git add --update'
 abbr gst "git status"
+abbr gba 'git branch -a'
+
+abbr gc 'git commit -v'
+abbr gc! 'git commit -v --amend'
+abbr gc!n 'git commit -v --amend --no-edit'
+abbr gco "git checkout"
 abbr gcp "git cherry-pick"
+abbr gcount 'git shortlog -sn'
+
+abbr gd 'git diff'
+abbr gdca 'git diff --cached'
+abbr gdcw 'git diff --cached --word-diff'
+abbr gdct 'git diff --staged'
+
+abbr gl 'git log'
+abbr gls 'git log --stat'
+abbr glp 'git log -p'
+
+abbr gr 'git remote -v'
+abbr gra 'git remote add'
+abbr grset 'git remote set-url'
+
+abbr grs 'git reset'
+abbr grs! 'git reset --hard'
+abbr grsh 'git reset HEAD'
+abbr grsh! 'git reset HEAD --hard'
+abbr gcleanall 'git reset --hard && git clean -dffx'
 
 # for loop to cd into directories
 abbr 4DIRS --set-cursor=! "$(string join \n -- 'for dir in */' 'cd $dir' '!' 'cd ..' 'end')"
