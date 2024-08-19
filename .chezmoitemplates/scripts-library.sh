7793c8530b33244397e16a4489ea30846492c0bd
magicwenli <48174882+magicwenli@users.noreply.github.com>
2024-08-19 09:36:01 +0000

    fix: skip op if not tiny distro

--- .chezmoitemplates/scripts-library.sh
+++ .chezmoitemplates/scripts-library.sh
@@ -3,6 +3,12 @@
 set -euo pipefail
 
 readonly root_homebrew_prefix="/home/linuxbrew/.linuxbrew"
+# export homebrew mirror
+export HOMEBREW_INSTALL_FROM_API=1
+export HOMEBREW_API_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api"
+export HOMEBREW_BOTTLE_DOMAIN="https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
+export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
 
 if [[ -n "${DOTFILES_DEBUG:-}" ]]; then
   set -x
